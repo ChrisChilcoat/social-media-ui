65a3af178144216f7b5e1bd78b39eacfe0de2506
Chris Chilcoat <cchilc01@gmail.com>
2021-09-02 16:47:55 -0400

    fixing home page link

--- src/components/site/Header.js
+++ src/components/site/Header.js
@@ -7,11 +7,11 @@ export default function SiteHeader() {
       <nav className="max-w-screen-xl mx-auto " aria-label="Top">
         <div className="flex items-center justify-between w-full py-3 border-b border-green-800 lg:border-none">
           <div className="flex items-center">
-            <a href="/" className="flex items-center">
+            <NavLink to="/" className="flex items-center">
               <span className="flex-1 ml-2 text-2xl font-extrabold text-white uppercase sm:text-3xl">
                 <span className="">Social Media</span> <span className="opacity-80">UI</span>
               </span>
-            </a>
+            </NavLink>
             <div className="hidden ml-10 space-x-8 lg:block">
               <NavLink to='/components' className='text-base font-medium text-white hover:text-indigo-50' activeClassName='text-indigo-50'>Components</NavLink>
               <NavLink to='/documentation' className='text-base font-medium text-white hover:text-indigo-50' activeClassName='text-indigo-50' exact>Documentation</NavLink>
